e468c124e3df5c5803002b55987a0d57d65eaced
akshatachoudhary <55664109+akshatachoudhary@users.noreply.github.com>
2019-12-12 20:57:10 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+s='William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the worlds greatest dramatist.'
+
+
+# In[2]:
+
+
+print(s)
+
+
+# In[3]:
+
+
+s="William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist."
+
+
+# In[4]:
+
+
+print(s)
+
+
+# In[5]:
+
+
+s='"William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist."'
+
+
+# In[6]:
+
+
+s="'William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist.'"
+
+
+# In[7]:
+
+
+print(s)
+
+
+# In[ ]:
+
+
+
+
